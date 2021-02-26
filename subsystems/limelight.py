b696380ea6758755eaf8b875a7700af08a891ac3
Ben Bistline <bbistline02@gmail.com>
2021-02-25 20:45:32 -0500

    Changed the limelight subsystem. Revert here if it's broken.

--- subsystems/limelight.py
+++ subsystems/limelight.py
@@ -12,48 +12,37 @@ class Limelight(CougarSystem):
     """Subsystem for interacting with the limelight."""
 
     def __init__(self):
-        super().__init__('Limelight')
+        super().__init__('limelight')
 
         self.nt = NetworkTables.getTable("limelight")
-        self.tv = Config("limelight/tv", 0)
-        self.tx = Config("limelight/tx", 0)
-        self.ty = Config("limelight/ty", 0)
-        self.ta = Config("limelight/ta", 0)
 
         self.driveTable = NetworkTables.getTable("DriveTrain")
 
-        # Set the range of velocities.
-        self.maxShooterRPM = 5600
-        self.minShooterRPM = 3800
-
         self.setPipeline(1)
 
-        self.closeShot = True
-
     def setPipeline(self, pipeline: int):
-        self.nt.putNumber("pipeline", pipeline)
+        self.put('pipeline', pipeline)
 
     def getY(self):
         # Return the x value to correct for the limelight being rotated.
-        return self.nt.getEntry("tx").getDouble(0)
+        return self.get("tx")
 
     def getX(self):
         # Return the y value to correct for the limelight being rotated.
-        return self.nt.getEntry("ty").getDouble(0)
+        return self.get("ty")
 
     def getA(self):
-        return self.nt.getEntry("ta").getDouble(0)
+        # Returns the area of the selected target.
+        return self.get("ta")
 
     def getTape(self):
         # Return whether or not tape is being detected by the limelight.
-        if self.nt.getEntry("tv").getDouble(0) == 1:
-            return True
-        return False
+        return self.get("tv") == 1
 
     def takeSnapShot(self):
         # Have the limelight take a snapshot.
         # These can be viewed by connecting to the limelight.
-        self.nt.putNumber("snapshot", 1)
+        self.put("snapshot", 1)
 
     # Not currently in use
     # def onTarget(self):
@@ -66,6 +55,3 @@ class Limelight(CougarSystem):
     #             return False
     #     else:
     #         return False
-
-    def updateNetworkTables(self):
-        pass
