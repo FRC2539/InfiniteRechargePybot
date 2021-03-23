059129859d1e26367032a47ff247ea3cbd965aaa
Ben Bistline <bbistline02@gmail.com>
2021-03-22 22:42:18 -0400

    Fixed or attempted to fix a logic error.

--- commands/drivetrain/segmentfollowercommand.py
+++ commands/drivetrain/segmentfollowercommand.py
@@ -4,25 +4,14 @@ import math
 
 import robot
 
-
 class SegmentFollowerCommand(CommandBase):
-    def __init__(
-        self,
-        waypoints: list,
-        angleTolerance=5,
-        maxSpeed=1,
-        deccelerate=False,
-        speedOffset=0,
-        kP=0.0325,
-        startingAngle=None,
-        stopWhenDone=True,
-    ):
+    def __init__(self, waypoints: list, angleTolerance=5, maxSpeed=1, deccelerate=False, speedOffset=0, kP=0.0325, startingAngle=None, stopWhenDone=True):
         """
         You start at [0, 0]. speedOffset is applied
         to the left hand side of the drivetrain! Note,
         this is not exactly accurate, but it is pretty consistent!
-        If you don't wanna correct the heading, leave kP as 0. If
-        you want to correct it relative to zero (instead of the captured
+        If you don't wanna correct the heading, leave kP as 0. If 
+        you want to correct it relative to zero (instead of the captured 
         starting angle) leave startingAngle as None.s
         """
         super().__init__()
@@ -76,24 +65,24 @@ class SegmentFollowerCommand(CommandBase):
                     theta = -90
 
             else:
-                if finalX > 0:  # Going to the right
+                if finalX > 0: # Going to the right
                     theta = ((math.atan2(finalY, finalX) * 180 / math.pi) + 90) % 180
-                else:  # Going to the left
+                else: # Going to the left
                     theta = -((math.atan2(finalX, finalY) * 180 / math.pi) % 180)
-
+                    
             distance = math.sqrt(finalX ** 2 + finalY ** 2)
 
             self.distances.append(distance)
             self.angles.append(theta)
-
-        print("d " + str(self.distances))
-        print("a " + str(self.angles))
+            
+        print('d ' + str(self.distances))
+        print('a ' + str(self.angles))
 
     def initialize(self):
         robot.drivetrain.setModuleProfiles(1, turn=False)
 
         self.pointTracker = 0
-
+        
         if self.startingAngle is None:
             self.startingAngle = 0
         else:
@@ -103,7 +92,7 @@ class SegmentFollowerCommand(CommandBase):
 
         self.desiredAngle = self.angles[0]
         self.desiredDistance = self.distances[0]
-
+        
         self.lastDisplacement = self.desiredDistance
 
         self.pathFinished = False
@@ -112,31 +101,31 @@ class SegmentFollowerCommand(CommandBase):
         self.passed = True
 
     def execute(self):
-
+        
         robot.drivetrain.setUniformModuleAngle(self.desiredAngle)
-
-        if self.atWaypoint():  # Watches to see if we pass the desired waypoint.
+        
+        if self.atWaypoint(): # Watches to see if we pass the desired waypoint.
             self.startPos = robot.drivetrain.getPositions()
             self.passed = True
-
-        if self.passed:  # Have we gone through the waypoint?
+        
+        if self.passed: # Have we gone through the waypoint?
 
             if self.notRanYet:  # Runs once at the waypoint.
 
-                try:  # If it can't index them, then it's done.
+                try: # If it can't index them, then it's done.
                     self.desiredAngle = self.angles[self.pointTracker]
                     self.desiredDistance = self.distances[self.pointTracker]
-
+                    
                     self.lastDisplacement = self.desiredDistance
 
-                except (IndexError):
+                except(IndexError):
                     self.pathFinished = True
                     return
-
+                
                 self.pointTracker += 1
-
+                
                 self.notRanYet = False
-
+                                
             self.count = 0
             if self.count != 4 and not self.moveSet:
                 for currentAngle in robot.drivetrain.getModuleAngles():
@@ -148,89 +137,44 @@ class SegmentFollowerCommand(CommandBase):
                     else:
                         continue
 
-            if self.count >= 2:  # All angles aligned.
+            if self.count >= 2:  # All angles aligned.                
                 # Add extra speed because the robot can't follow a damn straight line. My need Weaver's heading
-                # algo.
-                robot.drivetrain.setSpeeds(
-                    [
-                        self.maxSpeed + self.speedOffset,
-                        self.maxSpeed,
-                        self.maxSpeed + self.speedOffset,
-                        self.maxSpeed,
-                    ]
-                )
+                # algo. 
+                robot.drivetrain.setSpeeds([self.maxSpeed + self.speedOffset, self.maxSpeed, self.maxSpeed + self.speedOffset, self.maxSpeed])
                 self.moveSet = True
                 self.passed = False
 
         else:
             self.moveSet = False
             self.notRanYet = True
-
-            speedOffset = robot.drivetrain.getAngleTo(self.startingAngle) * self.kP
-
-            print("so " + str(speedOffset))
-
+            
+            speedOffset = robot.drivetrain.getAngleTo(self.startingAngle)*self.kP
+            
+            print('so ' + str(speedOffset))
+            
             if self.deccelerate:
-                if (
-                    abs(
-                        self.desiredDistance
-                        - abs(
-                            abs(
-                                sum(robot.drivetrain.getPositions()) / 4
-                                - sum(self.startPos) / 4
-                            )
-                        )
-                    )
-                    <= 18
-                ):  # Slow down when we are about eighteen inches from the goal.
+                if abs(self.desiredDistance - abs(sum(robot.drivetrain.getPositions()) / 4 - sum(self.startPos) / 4)) <= 18: # Slow down when we are about eighteen inches from the goal.
                     robot.drivetrain.setUniformModulePercent(0.25)
-                elif (
-                    abs(
-                        self.desiredDistance
-                        - abs(
-                            abs(
-                                sum(robot.drivetrain.getPositions()) / 4
-                                - sum(self.startPos) / 4
-                            )
-                        )
-                    )
-                    <= 36
-                ):
+                elif abs(self.desiredDistance - abs(sum(robot.drivetrain.getPositions()) / 4 - sum(self.startPos) / 4)) <= 36: 
                     robot.drivetrain.setUniformModulePercent(0.5)
                 else:
-                    robot.drivetrain.setSpeeds(
-                        [
-                            self.maxSpeed + speedOffset,
-                            self.maxSpeed,
-                            self.maxSpeed + speedOffset,
-                            self.maxSpeed,
-                        ]
-                    )
-
+                    robot.drivetrain.setSpeeds([self.maxSpeed + speedOffset, self.maxSpeed, self.maxSpeed + speedOffset, self.maxSpeed])
+            
             else:
-
-                robot.drivetrain.setSpeeds(
-                    [
-                        self.maxSpeed + speedOffset,
-                        self.maxSpeed,
-                        self.maxSpeed + speedOffset,
-                        self.maxSpeed,
-                    ]
-                )
+                
+                robot.drivetrain.setSpeeds([self.maxSpeed + speedOffset, self.maxSpeed, self.maxSpeed + speedOffset, self.maxSpeed])
 
     def atWaypoint(self):
         count = 0
         for position, start in zip(robot.drivetrain.getPositions(), self.startPos):
-            if abs(position - (start + self.desiredDistance)) < 1 or abs(
-                (position - (start + self.desiredDistance))
-            ) > abs(
-                self.lastDisplacement
+            if (
+                (abs(position - (start + self.desiredDistance)) < 1) or (abs(position - start) >= self.desiredDistance)
             ):  # 1 inch is the tolerance, or have we passed it?
-                print("l " + str(self.lastDisplacement))
-                print(" current " + str(abs(position - (start + self.desiredDistance))))
-                print("\n\nAt waypoint")  # NOTE: It's distance remaining.
+                print('l ' + str(self.lastDisplacement))
+                print(' current ' + str(abs(position - (start + self.desiredDistance))))
+                print('\n\nAt waypoint') # NOTE: It's distance remaining.
                 return True
-
+        
         self.lastDisplacement = abs(position - (start + self.desiredDistance))
         return False
 
