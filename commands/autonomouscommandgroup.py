f7947eec4ae967df4d3a6e6afc12fb561d43d689
Ben <bbistline02@gmail.com>
2021-10-14 21:45:38 -0400

    comp tweaks

--- commands/autonomouscommandgroup.py
+++ commands/autonomouscommandgroup.py
@@ -6,6 +6,8 @@ from commands.drivetrain.turncommand import TurnCommand
 from commands.drivetrain.turntocommand import TurnToCommand
 from commands.drivetrain.movecommand import MoveCommand
 
+from commands.limelight.automatedshootcommand import AutomatedShootCommand
+
 # from commands.limelight.automatedshootcommand import AutomatedShootCommand
 
 from networktables import NetworkTables
@@ -52,19 +54,19 @@ class AutonomousCommandGroup(SequentialCommandGroup):
         """
         Just move back off the line.
         """
-        self.addCommands(
-            MoveCommand(42)
-        )
-        
-    def shootFirstThree(self):
+        self.addCommands(MoveCommand(42))
+
+    def AshootFirstThree(self):
         """
         Move off the line and shoot 3 balls.
         """
         self.addCommands(
+            InstantCommand(lambda: robot.shooter.setRPM(3300), [robot.shooter]),
+            AutomatedShootCommand(3300).withTimeout(10),
             MoveCommand(42),
-            AutomatedShootCommand(3300),
+            # AutomatedShootCommand(3300),
         )
-        
+
     def trenchSixBall(self):
         """
         Shoot 3 balls, then collect the rest of the balls from the trench. Move to a safe location and shoot.
@@ -75,13 +77,10 @@ class AutonomousCommandGroup(SequentialCommandGroup):
             AimWithLimelightCommand(),
             AutomatedShootCommand(3300).withTimeout(4),
             InstantCommand(lambda: robot.shooter.setRPM(3300), [robot.shooter]),
-            InstantCommand(
-                lambda: robot.ballintake.forwardIntake, [robot.ballintake]
-            ),
+            InstantCommand(lambda: robot.ballintake.forwardIntake, [robot.ballintake]),
             MoveCommand(120),
             TurnCommand(10),
             MoveCommand(-135),
             AimWithLimelightCommand(),
             AutomatedShootCommand(3300),
         )
-            
