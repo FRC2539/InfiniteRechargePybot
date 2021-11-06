c81f6a8374fd9089fce1c137c82c731f619752ec
unknown <brbistli@mtu.edu>
2021-11-06 15:33:07 -0400

    did some work on a crappy library computer. Going to plug in and test.

--- custom/rref.py
+++ custom/rref.py
@@ -11,24 +11,61 @@ class Matrix:
     """
     
     def __init__(self, matrix):
-        self.rows = []
-        self.numOfPivots = len(matrix[0]) - 1
+        self.m = []
+        self.pivots = len(matrix[0]) - 1           # Because of augmentation
+        self.rows = len(matrix)
         for row in matrix:
-            self.rows.append(Row(row))
+            self.m.append(Row(row))
             
     def solve(self):
         self.rearrange()
         self.display()
-        
+
+        if self.rows < self.pivots:
+            self.pivots = self.rows
+
+        for pivotSelector in range(self.pivots):
+            selectedRow = self.m[pivotSelector]
+            a = selectedRow.getValue(pivotSelector)
+            for i in range(pivotSelector+1, self.rows):         # This line might break it.
+                rowBelow = self.m[i]
+                b = rowBelow.get(pivotSelector)
+                conversion = (b / a) * -1
+                invConversion = (1 / conversion)
+                selectedRow.scale(conversion)
+                result = rowBelow.combine(selectedRow)
+                self.m[i] = result
+                selectedRow.scale(invConversion)
+
+        for pivotSelector in range(self.pivots):
+            selectedRow = self.m[pivotSelector]
+            a = selectedRow.getValue(pivotSelector)
+            for i in range(pivotSelector):         # This line might break it.
+                rowAbove = self.m[i]
+                b = rowAbove.get(pivotSelector)
+                conversion = (b / a) * -1
+                invConversion = (1 / conversion)
+                selectedRow.scale(conversion)
+                result = rowAbove.combine(selectedRow)
+                self.m[i] = result
+                selectedRow.scale(invConversion)
+
+        for i in self.m:
+            if not i.isEmpty():
+                selectedRow = self.m[i]
+                pivot = selectedRow.getValue(i)
+                selectedRow.scale(1 / pivot)
+
+        self.display()
                             
     def display(self):
         print('------')
-        for row in self.rows:
+        for row in self.m:
             print(row.getRow())
         print('------')
         
     def rearrange(self):
-        self.rows.sort(key=lambda x: x.numOfZeros(), reverse=False)
+        self.m.sort(key=lambda x: x.numOfZeros(), reverse=False)
 
                     
 class Row:
@@ -51,6 +88,9 @@ class Row:
     def getRow(self):
         return self.columns
     
+    def isEmpty(self):
+        return self.numOfZeros() == len(self.columns)
+
     def numOfZeros(self):
         return self.columns.count(0)
         
